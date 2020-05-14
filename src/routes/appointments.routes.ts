8bb413406d57decc15597fcbc6a4fe14a25d0d7e
Assumps <jrvitor5@gmail.com>
2020-05-13 23:57:18 -0300

    Utilizando repository para divisao de camada de responsabilidades da aplicação, dividindo as responsabilidades da model e das rotas de agendamentos

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
-import Appointment from '../models/Appointments';
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentRepository';
 
 const appointmentsRouter = Router();
 
-const appointments: Appointment[] = [];
+const appointmentRepository = new AppointmentsRepository();
 
 // Post http://localhost:3333/appointments
 appointmentsRouter.post('/', (request, response) => {
@@ -12,8 +12,8 @@ appointmentsRouter.post('/', (request, response) => {
 
     const parsedDate = startOfHour(parseISO(date));
 
-    const findAppointmentInSameDate = appointments.find(appointment =>
-        isEqual(parsedDate, appointment.date),
+    const findAppointmentInSameDate = appointmentRepository.findByDate(
+        parsedDate,
     );
 
     if (findAppointmentInSameDate) {
@@ -22,9 +22,7 @@ appointmentsRouter.post('/', (request, response) => {
             .json({ message: 'This appointment is already booked' });
     }
 
-    const appointment = new Appointment(provider, parsedDate);
-
-    appointments.push(appointment);
+    const appointment = appointmentRepository.create(provider, parsedDate);
 
     return response.json(appointment);
 });
